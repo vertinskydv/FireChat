6bd2df12add0f585df760310e8ff3315ed3a4b1d
Dmitry Vertinsky <vertinskydv@gmail.com>
2017-06-13 19:07:20 +0300

    start the implementation of a redux store

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -1,2 +1,3 @@
 // Chat Action Constants
 export const ADD_NEW_CHAT = 'ADD_NEW_CHAT';
+export const CHANGE_USER_STATUS = 'CHANGE_USER_STATUS';
--- src/app/store/chatState.ts
+++ src/app/store/chatState.ts
@@ -1,11 +1,12 @@
 import { ActionReducer, Action } from '@ngrx/store';
-import { ADD_NEW_CHAT } from './actions';
+import { ADD_NEW_CHAT, CHANGE_USER_STATUS } from './actions';
 
 export const chatState = (state: any = {}, action: Action) => {
   switch (action.type) {
     case ADD_NEW_CHAT:
-      console.log(action);
       return Object.assign({}, state, {'chatName': action.payload});
+
+
 
     default:
       return state;
