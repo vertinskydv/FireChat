646efa9d6f8fb9a3d510730b31977e8b7b0ca720
Dmitry Vertinsky <vertinskydv@gmail.com>
2017-06-14 09:54:29 +0300

    fix bud

--- src/app/store/chatState.ts
+++ src/app/store/chatState.ts
@@ -1,7 +1,7 @@
 import { ActionReducer, Action } from '@ngrx/store';
 import { ADD_NEW_CHAT, CHANGE_USER_STATUS } from './actions';
 
-export const chatState = (state: any = {}, action: Action) => {
+export function chatState (state: any = {}, action: Action) {
   switch (action.type) {
     case ADD_NEW_CHAT:
       return Object.assign({}, state, {'chatName': action.payload});
