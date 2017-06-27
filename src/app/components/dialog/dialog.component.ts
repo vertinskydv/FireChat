1dc56402c376d27d0c7b262fd02e0f356510d511
Dmitry Vertinsky <vertinskydv@gmail.com>
2017-06-27 18:54:21 +0300

    .

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -8,7 +8,37 @@ import { MdDialogRef } from '@angular/material';
 })
 export class AddNewChatDialog implements OnInit {
 
-  constructor(dialogRef: MdDialogRef<AddNewChatDialog> ) { }
+  errors = {
+    name: false,
+    members: false
+  };
+
+  dialogref: MdDialogRef<AddNewChatDialog>;
+  nameValue: string;
+  constructor(dialogRef: MdDialogRef<AddNewChatDialog> ) {
+    this.dialogref = dialogRef;
+  }
+
+  onKeyUpName(value) {
+    this.nameValue = value;
+    if (value) {
+      this.errors.name = false;
+    }
+  }
+
+  onClickCancel() {
+    this.dialogref.close();
+  }
+
+  onClickCreate() {
+    debugger;
+    if (this.nameValue.search(/\S/g) !== -1) {
+      console.log('chat created!');
+    } else {
+      this.errors.name = true;
+    }
+
+  }
 
   ngOnInit() {
   }
